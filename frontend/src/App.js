849adcaef9ffa7b65eea0303dd901c6677fa6855
ericsheng495 <eric.sheng495@gmail.com>
2022-09-27 11:26:48 -0400

    set up pages and routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
+import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
 
 function App() {
-  return <h1>Let's start!</h1>;
+  return (
+    <div>
+      <BrowserRouter>
+        {/* switch = if-else route render */}
+        <Switch>
+          <Route path="/" exact>
+            <Users></Users>
+          </Route>
+          <Route path="/places/new" exact>
+            <NewPlace></NewPlace>
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
